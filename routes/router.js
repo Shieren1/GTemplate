0dcc3c1af58f3e695fce4c3721f2c587bd05d114
KllenCaringal <163014706+KllenCaringal@users.noreply.github.com>
2024-10-01 22:44:49 +0800

    Update

--- routes/router.js
+++ routes/router.js
@@ -5,11 +5,9 @@ const sma = require('../controller/sma_controller');
 
 router.get('/', sma.index);
 router.get('/products', sma.products);
-<<<<<<< Updated upstream
+
 //router.post('/save', sma.save);
-=======
 router.get('/prodDetail', sma.prodDetail);
->>>>>>> Stashed changes
 
 
 module.exports = router;
