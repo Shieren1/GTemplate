48c3101e0058e74c4fb8503baf0e86f9f2827bab
KllenCaringal <163014706+KllenCaringal@users.noreply.github.com>
2024-10-08 16:13:08 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const routes = require('./routes/router');
 const session = require('express-session');
 const app = express();
 
-// Set up session
 app.use(session({
     secret: 'yourSecretKey',
     resave: false,
@@ -12,22 +11,11 @@ app.use(session({
     cookie: { secure: false } 
 }));
 
-// Serve static files
 app.use(express.static('public'));
-
-// Set EJS as the view engine
 app.set('view engine', 'ejs');
-
-// Use body-parser to parse URL-encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Use router for handling routes
 app.use('/', routes);
-
-// Log the views directory for debugging
 console.log('Views Directory:', app.get('views'));
-
-// Start the server
 app.listen(8080, () => {
     console.log('Server initialized on http://localhost:8080');
 });
